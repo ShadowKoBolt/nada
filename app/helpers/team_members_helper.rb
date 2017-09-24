dd35d50ce648ac803232dfb8d511931be9a444ad
Nick Bolt <nick.bolt@redemptionmedia.co.uk>
2017-09-24 21:20:21 +0100

    💄 Align team members

--- app/helpers/team_members_helper.rb
+++ app/helpers/team_members_helper.rb
@@ -6,4 +6,12 @@ module TeamMembersHelper
       return :div, class: 'team-member'
     end
   end
+
+  def offset_class(group, _size)
+    group_size = group.compact.size
+    return 'col-center' if group_size == 1
+    return 'col-sm-offset-3' if group_size == 2
+    return 'col-sm-offset-1' if group_size == 3
+    ''
+  end
 end
