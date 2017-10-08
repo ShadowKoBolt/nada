6ec7bf9e043a9a725cb24e1f74880faf3478df18
Nick Bolt <nick.bolt@redemptionmedia.co.uk>
2017-10-08 12:30:21 +0100

    ✏️  Add improver to dance class levels

--- app/models/dance_class.rb
+++ app/models/dance_class.rb
@@ -1,7 +1,7 @@
 class DanceClass < ApplicationRecord
   STYLE_OPTIONS = ['Egyptian Belly Dance', 'Fusion', 'Turkish Belly Dance', 'Tribal ATS', 'Gothic Belly Dance',
                    'Tribal Fusion', 'Folkloric', 'Gulf', 'Iraqi', 'Gypsy'].freeze
-  LEVEL_OPTIONS = ['Open level', 'Beginner', 'Intermediate', 'Advanced'].freeze
+  LEVEL_OPTIONS = ['Open level', 'Beginner', 'Improver', 'Intermediate', 'Advanced'].freeze
   DISTANCE_OPTIONS = [['Within 50 miles', 50],
                       ['Within 25 miles', 25],
                       ['Within 10 miles', 10],
